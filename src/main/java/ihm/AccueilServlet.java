c20020302733da54b3762a356d0c3c1fc7921ef2
Sebastien GLIPPA <sebastien.glippa2023@campus-eni.fr>
2023-05-09 09:40:50 +0200

    modif header suite au merge

--- src/main/java/ihm/AccueilServlet.java
+++ src/main/java/ihm/AccueilServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
+import bo.Utilisateur;
+
 @WebServlet("")
 public class AccueilServlet extends HttpServlet {
   
@@ -16,10 +18,10 @@ public class AccueilServlet extends HttpServlet {
 	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-//    	 HttpSession session = request.getSession();
-//         Utilisateur utilisateur = new Utilisateur();
-//         utilisateur.setPrenom("gilbert");
-//         session.setAttribute("utilisateur",utilisateur);
+    	 HttpSession session = request.getSession();
+         Utilisateur utilisateur = new Utilisateur();
+         utilisateur.setPrenom("gilbert");
+         session.setAttribute("utilisateur",utilisateur);
         
         request.getRequestDispatcher("/WEB-INF/encheres/accueil.jsp").forward(request,response);
     }
