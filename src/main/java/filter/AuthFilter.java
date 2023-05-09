57586e9960169a57b6f268a212e04cc4ac0b5e93
Sebastien GLIPPA <sebastien.glippa2023@campus-eni.fr>
2023-05-09 16:32:20 +0200

    avant merge

--- src/main/java/filter/AuthFilter.java
+++ src/main/java/filter/AuthFilter.java
@@ -1,21 +1,22 @@
 package filter;
 
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import bo.Utilisateur;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.IOException;
-
-import bo.Utilisateur;
 
 @WebFilter (
 		dispatcherTypes = {
@@ -31,7 +32,6 @@ import bo.Utilisateur;
 
 public class AuthFilter extends HttpFilter implements Filter {
 
-
 	
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// place your code here
@@ -40,12 +40,16 @@ public class AuthFilter extends HttpFilter implements Filter {
 		
 		//GUARD
 		if (utilisateur == null) {
-			//TODO a confirmer pour la redirection accueil ou login
-			response.sendRedirect(request.getContextPath()+"");
+			//TODO a confirmer pour la redirection accueil ou login	
+			//Boîte du message d'information
+		     
+			response.sendRedirect(request.getContextPath()+"");	
+			 
 			return;
+			
 		}
 		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		chain.doFilter(request, response);		
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
