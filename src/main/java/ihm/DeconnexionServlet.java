aa3e72d2a2c6b2d14b7445e36eb3fda545639fb0
Flegoff2318 <96115160+Flegoff2318@users.noreply.github.com>
2023-05-12 10:04:52 +0200

    suppression des tokens sur deconnexion

--- src/main/java/ihm/DeconnexionServlet.java
+++ src/main/java/ihm/DeconnexionServlet.java
@@ -1,16 +1,21 @@
 package ihm;
 
+import bll.TokenManager;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
+import bo.Utilisateur;
+
 @WebServlet("/deconnexion")
 public class DeconnexionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+        supprimerCookies(utilisateur,request,response);
         session.invalidate();
         response.sendRedirect(request.getContextPath());
     }
@@ -18,5 +23,16 @@ public class DeconnexionServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
+    }
+    public void supprimerCookies(Utilisateur utilisateur,HttpServletRequest request,HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies
+             ) {
+            if(cookie.getName().equals("userToken") || cookie.getName().equals("passToken")){
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        TokenManager.getInstance().supprimerToken(utilisateur.getNoUtilisateur());
     }
 }
